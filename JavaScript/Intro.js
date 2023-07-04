fdff29157924efb0f895de889789660a75d03d02
Glory Gupta <prikarshglory@gmail.com>
2023-07-04 21:30:40 +0530

    Update Intro.js

--- JavaScript/Intro.js
+++ JavaScript/Intro.js
@@ -1,1 +1,11 @@
+// printing
 console.log("hey JS");
+
+//Declare a variable
+let a;  // default value of any variable is undefined
+a=99;
+a=9.44;
+a="Hello I'm a string";
+a="Hello I'm also a string";
+//We can update the values of variables
+console.log("variable contains", a);
