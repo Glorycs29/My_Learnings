fc9be164a353f6f75ad9a4bebaebebf2cbf9cf51
Glory Gupta <prikarshglory@gmail.com>
2023-07-04 21:33:25 +0530

    Create #1 variable.js

---
+++ JavaScript/Variable/#1 variable.js
@@ -0,0 +1,11 @@
+//Declare a variable
+let a;  // default value of any variable is undefined
+a=99;
+a=9.44;
+a="Hello I'm a string";
+a="Hello I'm also a string";
+//We can update the values of variables
+console.log("variable contains", a);
+
+//Types of variables: Premitive: number, string, boolena, null
+
