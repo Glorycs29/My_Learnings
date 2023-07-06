10045919140c08aca0cea50a02805d33cfccefae
Glory Gupta <prikarshglory@gmail.com>
2023-07-06 18:35:17 +0530

    Add files via upload

---
+++ JavaScript/Object/#2_objects.js
@@ -0,0 +1,12 @@
+let js={
+    name:"JavaScript",
+    abbreviation: "JS",
+    isAwesome: true,
+    officialSpec: "ECMAScript",
+    birthYear: 1995,
+    creator: "Brendan Eich"
+};
+
+if(js.isAwesome){
+    console.log("Hell yes JS!");
+}
