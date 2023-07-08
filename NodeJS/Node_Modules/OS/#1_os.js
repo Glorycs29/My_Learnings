a4dc23bf3dcd91f2d24507b4f5c97e8c55a7e656
Glory Gupta <prikarshglory@gmail.com>
2023-07-08 13:28:21 +0530

    Add files via upload

---
+++ NodeJS/Node_Modules/OS/#1_os.js
@@ -0,0 +1,5 @@
+let os=require("os");
+//console.log(os.arch()); // for printing the architechture
+ //console.log(os.platform()); // platform
+//console.log(os.networkInterfaces()); // network interfaces
+console.log(os.cpus());
