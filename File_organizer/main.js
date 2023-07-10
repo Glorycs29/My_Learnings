efb7048226e0bc92dcc62956fefbf52d905e401b
Glory Gupta <prikarshglory@gmail.com>
2023-07-10 12:01:54 +0530

    Update main.js

--- File_organizer/main.js
+++ File_organizer/main.js
@@ -3,3 +3,34 @@ let inputArr = process.argv.slice(2);
  // node main.js tree "directoryPath"
  // node main.js organize "directoryPath"
  // node main.js help
+let command = inputArr[0];
+switch(command){
+	case "tree":
+		treeFn(inputArr[1]);
+		break;
+	case "organize":
+		organizeFn(inputArr[1]);
+		break;
+	case "help":
+		helpFn()
+		break;
+	default:
+		console.log("Please input right command");
+			break;	
+}
+
+function treeFn(dirPath){
+	console.log("Tree command implementation for", dirPath);
+}
+function organizeFn(dirPath){
+	console.log("Organize command implementation for", dirPath);
+}
+function helpFn(){
+	console.log(
+		`List of All commands:
+				node main.js tree "directoryPath"
+ 				node main.js organize "directoryPath"
+ 				node main.js help
+				`);
+}
+
