8442fe7743acb8ee4579c4f1f56ded74228ada50
Glory Gupta <prikarshglory@gmail.com>
2023-07-25 08:31:00 +0530

    Create # 0_functions.js

---
+++ JavaScript/Functions/# 0_functions.js
@@ -0,0 +1,19 @@
+function real(){
+    console.log("Hi i am real, always run me");
+}
+
+function real(){
+    console.log("NO i am real, always run me 🥲");
+}
+
+function real(){
+    console.log("Bhaap!! I am real, always run me 😎😎");
+}
+
+real();
+
+
+// Output:
+
+// [Success] Your code was executed successfully
+// Bhaap!! I am real, always run me
