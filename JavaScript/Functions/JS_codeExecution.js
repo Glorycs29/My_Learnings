cb5693139ee661701c45e3e394805dc54822bc08
Glory Gupta <prikarshglory@gmail.com>
2023-07-26 11:59:58 +0530

    Create JS_codeExecution.js

---
+++ JavaScript/Functions/JS_codeExecution.js
@@ -0,0 +1,28 @@
+function real(){
+    console.log("I am real");
+}
+
+function real(){
+    console.log("No, im real");
+}
+real();
+function real(){
+    console.log("You both are such a drag!!");
+}
+
+// o/p : You both are such a drag!!
+
+
+function real(){
+    console.log("I am real");
+}
+
+function real(){
+    console.log("No, im real");
+}
+function real(){
+    console.log("You both are such a drag!!");
+}
+real();
+
+// o/p : You both are such a drag!!
