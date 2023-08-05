8c60715bade79022d1155e94fc77ecb59a5ee0e1
Glory Gupta <prikarshglory@gmail.com>
2023-08-05 10:41:06 +0530

    Create #1 Scope.js

---
+++ JavaScript/Let and Const/#1 Scope.js
@@ -0,0 +1,18 @@
+let letFruit = "orange";
+var varFruit = "orange";
+console.log("letFruit: ", letFruit, "varFruit: ", varFruit);
+{
+    let letFruit = "apple";
+    var varFruit = "strawberry";
+    console.log("letFruit: ", letFruit, "varFruit: ", varFruit);
+
+}
+console.log("letFruit: ", letFruit, "varFruit: ", varFruit);
+
+/*
+----------------------------------------------
+[Success] Your code was executed successfully
+letFruit:  orange varFruit:  orange
+letFruit:  apple varFruit:  strawberry
+letFruit:  orange varFruit:  strawberry
+*/
