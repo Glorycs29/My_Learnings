0d7bb488e45f9f861a6716839a9a3f182db88d80
Glory Gupta <prikarshglory@gmail.com>
2023-08-08 14:14:23 +0530

    Create Code.js

---
+++ NodeJS/Chi-color module/Code.js
@@ -0,0 +1,9 @@
+// console.log("Hello");
+
+const color = require('cli-color')
+console.log(color.yellow('Hello Node js'))
+
+console.log(color.green('Hello Node js'))
+
+
+console.log(color.red('Hello Node js'))
