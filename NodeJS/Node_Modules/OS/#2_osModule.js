4967be6ae8c4893ba2d66bee049ab035ac32ba62
Glory Gupta <prikarshglory@gmail.com>
2023-08-08 15:59:25 +0530

    Create #2_osModule.js

---
+++ NodeJS/Node_Modules/OS/#2_osModule.js
@@ -0,0 +1,19 @@
+const os= require('os')
+
+console.log('Os type: ', os.type())
+
+console.log('Os platfrom: ', os.platform())
+
+console.log('CPU architecture: ', os.arch())
+
+console.log('Os details: ', os.cpus())
+
+console.log('Free memory: ', os.freemem())
+
+console.log('Total memory: ', os.totalmem())
+
+console.log('CPU uptime: ', os.uptime())
+
+
+
+
